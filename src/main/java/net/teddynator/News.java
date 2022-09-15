86c1fb2005938ebfea31fda5ecd3f5cf4305c90b
ElerWohlmuthFH <eler.wohlmuth@gmail.com>
2022-09-15 06:18:52 -0700

    added language option

--- src/main/java/net/teddynator/News.java
+++ src/main/java/net/teddynator/News.java
@@ -2,11 +2,16 @@ package net.teddynator;
 
 import com.kwabenaberko.newsapilib.NewsApiClient;
 import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
+import com.kwabenaberko.newsapilib.models.request.SourcesRequest;
 import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
 import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+import com.kwabenaberko.newsapilib.models.response.SourcesResponse;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class News extends ListenerAdapter {
 
@@ -22,17 +27,26 @@ public class News extends ListenerAdapter {
                 searchTerm = searchTerm + " " + args[i];
             }
 
+            String language;
+            String[] languages = {"de", "en"};
 
+
+            if (Arrays.asList(languages).contains(args[args.length - 2]))
+//                    || !isNumeric(args[args.length - 1]) && args[args.length - 1].length() == 2)
+            {
+                language = args[args.length - 2];
+            } else if (Arrays.asList(languages).contains(args[args.length - 1])) {
+                language = args[args.length - 1];
+
+            } else {
+                language = "en";
+            }
             //checks if last arg is numeric
             if (isNumeric(args[args.length - 1])) {
                 int results = Integer.parseInt(args[args.length - 1]);
-                news(event.getChannel().asTextChannel(), searchTerm, results);
-            }
-//            else if(args.length==2){
-//                news(event.getChannel().asTextChannel(), searchTerm, 3);
-//            }
-            else if (args[args.length - 1] != null || !isNumeric(args[args.length - 1])) {
-                news(event.getChannel().asTextChannel(), searchTerm, 5); //default result count
+                news(event.getChannel().asTextChannel(), searchTerm, language, results);
+            } else if (args[args.length - 1] != null || !isNumeric(args[args.length - 1])) {
+                news(event.getChannel().asTextChannel(), searchTerm, language, 5); //default result count
             }
         }
     }
@@ -50,17 +64,16 @@ public class News extends ListenerAdapter {
     }
 
 
-    public void news(TextChannel channel, String keyword, int results) {
+    public void news(TextChannel channel, String keyword, String language, int results) {
 
         String key = "812717347b1b42eb91d185b0f0f9285c";
         NewsApiClient newsApiClient = new NewsApiClient(key);
-
 
         newsApiClient.getEverything(
 
                 new EverythingRequest.Builder()
                         .q(keyword)
-                        .language("en")
+                        .language(language)
                         .build(),
                 new NewsApiClient.ArticlesResponseCallback() {
                     @Override
@@ -69,7 +82,7 @@ public class News extends ListenerAdapter {
 
                         try {
                             for (int i = 0; i < results; i++) {
-                                String news = response.getArticles().get(i).getTitle();
+                                String news = response.getArticles().get(i).getUrl();
                                 channel.sendMessage(news).queue();
                             }
                         } catch (IndexOutOfBoundsException e) {
